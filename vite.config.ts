88e148f386769a5799bd7b4ff346f2f4d1d3e1a6
eden <198768181+lcweden@users.noreply.github.com>
2025-05-07 13:52:36 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { readFileSync } from "node:fs";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,13 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-    https: {
-      key: readFileSync("./secret/dev.server.key"),
-      cert: readFileSync("./secret/dev.server.chain.crt"),
-    },
-  },
   base: "/well-path-demo/",
 });
